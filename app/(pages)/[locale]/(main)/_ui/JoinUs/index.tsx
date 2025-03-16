b2b1b6b80bb6a44f52aac9fd3baee91b9f21c3ca
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-03-16 09:56:06 +0200

    change: joinUs

--- app/(pages)/[locale]/(main)/_ui/JoinUs/index.tsx
+++ app/(pages)/[locale]/(main)/_ui/JoinUs/index.tsx
@@ -3,7 +3,6 @@ import { useTranslations } from 'next-intl'
 import type { FC } from 'react'
 import { links } from '@shared/const/links'
 import Button from '@shared/ui/Button'
-import Each from '@shared/ui/Each'
 import ResponsiveImage from '@shared/ui/ResponsiveImage'
 import Space from '@shared/ui/Space'
 import Title from '@shared/ui/Title'
@@ -16,36 +15,17 @@ import s from './JoinUs.module.scss'
 const JoinUs: FC = () => {
     const t = useTranslations('JoinUs')
 
-    const list = [t('list.0'), t('list.1'), t('list.2')]
-
     return (
         <section className={s.main} id='join-us'>
-            <Space className={s.main_content} direction='vertical' justify='between' gap='6'>
-                <Title title='Join Our Closed Tutor' secondTitle='Focus Group!' badge={{ variant: 'blue', icon: 'user-add', text: 'Join us' }} titleColor='white'>
-                    <Space direction='vertical' gap='3'>
-                        <Text size='16' lineHeight='160' color='primary100'>
-                            {t('title')}
-                        </Text>
-                        <Space as='ul' direction='vertical'>
-                            <Each
-                                of={list}
-                                render={(item) => (
-                                    <li className={s.listItem}>
-                                        <Text size='16' weight='500' color='primary100' lineHeight='120'>
-                                            {item}
-                                        </Text>
-                                    </li>
-                                )}
-                            />
-                        </Space>
-                    </Space>
+            <Space className={s.main_content} gap='6' direction='vertical'>
+                <Title title={t('title')} badge={{ variant: 'blue', icon: 'user-add', text: 'Join us' }} titleColor='white'>
+                    <Text size='16' lineHeight='160' color='primary100'>
+                        {t('description')}
+                    </Text>
                 </Title>
                 <Space direction='horizontal' gap='6' sm={{ gap: '4' }}>
-                    <Button className={s.button} as={Link} href={links.bookCall} variant='border'>
-                        {t('button')}
-                    </Button>
-                    <Button className={s.button} as={Link} href={links.contactUs} variant='secondary'>
-                        {t('contactButton')}
+                    <Button className={s.button} as={Link} href={links.register} variant='secondary'>
+                        {t('joinUs')}
                     </Button>
                 </Space>
             </Space>
